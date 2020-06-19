dc72429c24ac3d5ec7aec78ee08e3c3a74aa2e6b
Riley Persily <persilyriley@yahoo.com>
2020-06-18 18:02:05 -0700

    add helmet

--- myApp.js
+++ myApp.js
@@ -16,6 +16,9 @@ var app = express();              // Do Not Edit
 // [Helmet](https://github.com/helmetjs/helmet) helps you secure your
 // Express apps by setting various HTTP headers.
 // Install the package, then require it.
+const express = require("express");
+const helmet = require("helmet");
+app.use(helmet());
 
 
 
